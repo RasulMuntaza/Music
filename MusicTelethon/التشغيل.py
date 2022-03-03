1e34bfbf83088706ff049ae0afe5a90a12955eae
Rasul-Muntazar <98656596+Rasul-Muntazar@users.noreply.github.com>
2022-03-04 01:48:18 +0300

    Update التشغيل.py

--- MusicTelethon/التشغيل.py
+++ MusicTelethon/التشغيل.py
@@ -67,7 +67,7 @@ async def ytdl(link):
     else:
         return 0, stderr.decode()
 
-@Client.on_message(filters.command(["تشغيل"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["شغل"], prefixes=f"{HNDLR}"))
 async def play(client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
@@ -113,7 +113,7 @@ async def play(client, m: Message):
             await m.reply("الرد على ملف صوتي أو إعطاء شيء للبحث")
         else:
             await m.delete()
-            huehue = await m.reply("**.. جاري البحث عزيزي**")
+            huehue = await m.reply("**جاري البحث عزيزي ..**")
             query = m.text.split(None, 1)[1]
             search = ytsearch(query)
             if search == 0:
